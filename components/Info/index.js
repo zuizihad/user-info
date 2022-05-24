7c7d26f29747c565a6a61dd6e0ba7b320e94831c
zuizihad <ahmedxihad@gmail.com>
2022-05-24 22:39:24 +0600

    design fix

--- components/Info/index.js
+++ components/Info/index.js
@@ -9,9 +9,9 @@ const Info = ({ currentItems }) => {
                     {
                         currentItems && currentItems.map(item => (
                             <div className={styles['card']}>
-                                <h4>Full Name: {item.full_name}</h4>
-                                <h5>Email: {item.email}</h5>
-                                <h5>Phone: {item.phone}</h5>
+                                <h5>Full Name: {item.full_name}</h5>
+                                <h6>Email: {item.email}</h6>
+                                <h6>Phone: {item.phone}</h6>
                                 <h6>Address: {item.address}</h6>
                                 <h6>Date of birth: {item.date_of_birth}</h6>
                                 <span>Profession: {item.profession}</span>
